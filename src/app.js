ef8d2adedf554f5349d3dced41942bf0f01217c0
kiranbandi <bvenkatkiran@gmail.com>
2018-04-21 20:16:18 -0600

    intial asset setup complete

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
-import aws from "aws-sdk"
-console.log("ola")
+import $ from 'jquery';
+import swal from 'sweetalert';
+
+// sample link for image from s3 bucket
+// https://s3.ca-central-1.amazonaws.com/p2ircimagebucket/new/3.jpg
+
+
+
+$("#start-button").click(() => {
+    $(".start-button-panel").addClass("hidden");
+    $(".verification-panel").removeClass("hidden");
+})
